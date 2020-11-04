870010afbab6817ecb943e8a68c9ba7671bd215a
Claudiu.Sichet <sctamas@gmail.com>
2020-11-04 18:55:53 +0200

    202011041855

---
+++ src/main/java/Checkprime.java
@@ -0,0 +1,21 @@
+public class Checkprime {
+    public static void main(String[] args) {
+        int i = 0;
+        int start = 568;
+        int num = 0;
+        String primeNumbers = "";
+
+        for (i = 0; i < start; i++) {
+            int counter = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+                primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        System.out.println("Prime number(s) lower than start is (are) : " + primeNumbers);
+    }
+}
